ba3abc341b836b528cbadf6d184bf248f31af008
Sumukh Adiraju <sadiraju06@gmail.com>
2026-06-13 13:42:01 +0100

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        dict_s = {}
+        dict_t = {}
+        for char in s:
+            dict_s[char] = dict_s.get(char, 0) + 1
+        for char in t:
+            dict_t[char] = dict_t.get(char, 0) + 1
+
+        return dict_s == dict_t
