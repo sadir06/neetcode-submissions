3d6d9290ac87bd67f38226e2b0ac8c0e921679cf
Sumukh Adiraju <sadiraju06@gmail.com>
2026-06-15 19:37:40 +0100

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]: # Next instead of sorting every string, represent every string by the frequency of its characters, 2 strings are only identical if their frequencies are the same
+        res = defaultdict(list)
+
+        for s in strs:
+            strings = [0] * 26
+            for c in s:
+                strings[ord(c) - ord('a')] += 1
+            res[tuple(strings)].append(s)
+
+        return list(res.values())
