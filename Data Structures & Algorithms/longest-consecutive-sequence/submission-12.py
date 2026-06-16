8c192f501bafae219d640389e8d058f7371f66c9
Sumukh Adiraju <sadiraju06@gmail.com>
2026-06-16 15:37:37 +0100

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int: # Remember we don't have to build the list, but just find the longest consecutive sequence
+        store = set()
+        starting = []
+        longest_frequency = 0
+
+        for num in nums:
+            store.add(num)
+
+        for num in nums:
+            if num - 1 not in store:
+                starting.append(num)
+
+        for num in starting:
+            current = 1
+            while (num + 1) in store: # O(1)
+                current += 1
+                num += 1 
+            longest_frequency = max(current, longest_frequency) # Update it outside the for loop so that [0] is set to 1 instead of 0
+                
+        return longest_frequency
+
+        
