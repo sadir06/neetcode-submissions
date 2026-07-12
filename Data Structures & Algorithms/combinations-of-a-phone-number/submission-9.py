807f11173862d2bbf09d62c9a235b5e7e05a5519
Sumukh Adiraju <sadiraju06@gmail.com>
2026-07-12 18:45:39 +0100

    Add: combinations-of-a-phone-number - submission-9

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-9.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        self.results = []
+        self.current_path = []
+        digitToChar = { # I could NOT be bothered to type this out, so I just copied it from the solutions, because I knew we needed a mapping of the numbers to the letters
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        
+        def backtracking(i):
+            if i == len(digits):
+                self.results.append("".join(self.current_path))
+                return
+
+            for char in digitToChar[digits[i]]: 
+                self.current_path.append(char)
+                backtracking(i + 1) 
+                self.current_path.pop()
+
+        backtracking(0)
+        if self.results == [""]: # Essentially if we got an empty output, because we are adding "" into our list and they want us to return [], we just return that
+            return []
+
+        return self.results
