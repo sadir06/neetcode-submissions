690de8c3ebb455640ba06c73744823b402052a24
Sumukh Adiraju <sadiraju06@gmail.com>
2026-07-31 15:04:16 +0100

    Add: rotate-array - submission-8

---
+++ Data Structures & Algorithms/rotate-array/submission-8.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        if k == 0:
+            return
+
+        nums[:] = nums[-k:] + nums[:-k]
+        
