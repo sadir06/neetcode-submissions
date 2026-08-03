204fb6cab0cc7977795c77f17d2fc977392b9193
Sumukh Adiraju <sadiraju06@gmail.com>
2026-08-03 14:41:04 +0100

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        min_bound = max(weights) # O(n)
+        max_bound = sum(weights) # O(n)
+        total = sum(weights)
+        while min_bound < max_bound: # <= because they could eventually be the same
+            mid = min_bound + (max_bound - min_bound) // 2
+            total_weight = 0
+            j = 0
+            for i in range(days): # O(days * n) 
+                cur_weight = 0
+                while j < len(weights):
+                    if cur_weight + weights[j] > mid:
+                        break
+                    cur_weight += weights[j] # Add on the weights until we can't anymore
+                    j += 1 # Keep moving along the weights
+                total_weight += cur_weight # check how much weight has been collected 
+            if total_weight == total:
+                max_bound = mid # This is a valid mid bound
+            else:
+                min_bound = mid + 1 # Mid is not valid we need to skip it
+
+
+        return min_bound 
